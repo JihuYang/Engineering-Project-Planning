e19d9226e8cb26234678a2a20a038dac1b48d6f8
Jihu Yang <21900433@handong.edu>
2021-10-29 17:17:05 +0900

    create week10

---
+++ form/week10/json-form/src/components/elements/Color.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+import { FormContext } from '../../FormContext';
+
+const Color = ({ field_id, field_label, field_value }) => {
+    const { handleChange } = useContext(FormContext)
+    return (
+        <div class="input-group-prepend m-3">
+            <label htmlFor="favcolor" className="form-label">{field_label}</label>
+            <input type="color" className="form-control" id="favcolor"
+                value={field_value}
+                onChange={event => handleChange(field_id, event)}
+            />
+        </div>
+        
+    )
+}
+
+export default Color
